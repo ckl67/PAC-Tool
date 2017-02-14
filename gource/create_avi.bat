1d5e1663f09deec552216df389fb87d1f627e04a
ckl67 <christian.klugesherz@gmail.com>
2017-02-14 19:10:29 +0100

    only needed lib

--- gource/create_avi.bat
+++ gource/create_avi.bat
@@ -1,11 +1,21 @@
-echo "======================================================="
-echo "               TO KEEP WINDOWS OPEN					 "
-echo "======================================================="
-REM gource -1280x720 -s 1 -o gource.ppm \workspace\pac-tool\src
-gource -720x360 -s 1 -o gource.ppm ..\src
-ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i gource.ppm -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 gource.x264.avi
-echo " Will delete ppm"
-del *.ppm
-echo "======================================================="
-echo "    					Finish			 	 			 "
-echo "======================================================="
+@echo off
+cls
+echo =======================================================
+echo                       GOURCE
+echo =======================================================
+echo   Will generate .avi file of the Software projects 
+echo   displayed as an animated tree. 
+echo del .ppm
+REM -1280x720 not working to much to compute.
+REM CALL gource -1280x720 -s 1 -o gource.ppm ..\src
+REM CALL gource -720x360 -s 1 -o gource.ppm ..\src
+CALL gource -960x540 -s 1 -o gource.ppm ..\src
+echo generation of .avi
+@echo on
+ffmpeg -y -r 60 -i audio.mp3 -f image2pipe -vcodec ppm -i gource.ppm -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 -shortest gource.x264.avi
+@echo off
+echo del .ppm
+del "gource.ppm"
+echo =======================================================
+echo                        Finish			 	 			 
+echo =======================================================
