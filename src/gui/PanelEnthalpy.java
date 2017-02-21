e799079c01c59c93e5152c499ed3ad1fcf25534f
ckl67 <christian.klugesherz@gmail.com>
2017-02-21 22:29:35 +0100

    better rendering

--- src/gui/PanelEnthalpy.java
+++ src/gui/PanelEnthalpy.java
@@ -25,6 +25,7 @@ import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.RenderingHints;
 import java.awt.event.InputEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -303,6 +304,8 @@ public class PanelEnthalpy extends JPanel {
 
 		super.paintComponent(g);
 		setBackground(Color.WHITE);
+
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
 		// -----------------------------------
 		// Apply a translation so that the drawing
