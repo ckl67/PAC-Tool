b3dcdc900c1b0fdfac105a148a17a48ceb647fa5
ckl67 <christian.klugesherz@gmail.com>
2017-03-02 11:28:16 +0100

    minor

--- gource/create_avi.bat
+++ gource/create_avi.bat
@@ -9,7 +9,7 @@ echo del .ppm
 REM -1280x720 not working to much to compute.
 REM CALL gource -1280x720 -s 1 -o gource.ppm ..\src
 REM CALL gource -720x360 -s 1 -o gource.ppm ..\src
-CALL gource -960x540 -s 1 -o gource.ppm ..\src
+CALL gource -960x540 -s 5 -o gource.ppm ..\src
 echo generation of .avi
 @echo on
 ffmpeg -y -r 60 -i audio.mp3 -f image2pipe -vcodec ppm -i gource.ppm -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 -shortest gource.x264.avi
