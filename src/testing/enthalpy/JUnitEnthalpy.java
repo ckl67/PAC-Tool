e0b5522703db25e14ce501f2ef541f3706c686e4
ckl67 <christian.klugesherz@gmail.com>
2017-11-28 18:36:16 +0100

    test

---
+++ src/testing/enthalpy/JUnitEnthalpy.java
@@ -0,0 +1,14 @@
+package testing.enthalpy;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JUnitEnthalpy {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
