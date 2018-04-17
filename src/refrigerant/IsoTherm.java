85f57e84b9fac1db02ecd95d07aa12c64e293eb9
ckl67 <christian.klugesherz@gmail.com>
2018-04-17 16:23:09 +0200

    first commit in new branch

--- src/refrigerant/IsoTherm.java
+++ src/refrigerant/IsoTherm.java
@@ -2,4 +2,10 @@ package refrigerant;
 
 public class IsoTherm {
 
+	private double IsoTherm_P0_Ref;
+	private double IsoTherm_T0_Ref;
+	private double IsoTherm_T0_Delta;
+	private double IsoTherm_H0_Ref;
+	private double IsoTherm_H0_Delta;
+	
 }
